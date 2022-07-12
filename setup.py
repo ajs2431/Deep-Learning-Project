032d83cb86d7f5a96c6c8eb68a19226d3e8528a6
ajs2431 <108826443+ajs2431@users.noreply.github.com>
2022-07-12 11:08:06 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name="Deep/Learning/Project",
+  author="Angela Sun",
+  author_email="angelamango20@gmail.com",
+  license="MIT License",
+  description="This is my capstone project for the Big Data and Machine Learning program.",
+  packages=["DeepLearning"]
