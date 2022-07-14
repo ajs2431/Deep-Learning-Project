1c80de90908c732163a4935a640b03163c8f830d
ajs2431 <108826443+ajs2431@users.noreply.github.com>
2022-07-14 15:45:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-  name="Deep/Learning/Project",
+  name="Capstone-Project",
   author="Angela Sun",
   author_email="angelamango20@gmail.com",
   license="MIT License",
   description="This is my capstone project for the Big Data and Machine Learning program.",
-  packages=["DeepLearning"]
+  packages=["Capstone-Project"]
 )
